acd27501201fd0e9a5fd276173038770b4f39a07
thd0222 <thdtjddb@naver.com>
2019-04-15 19:46:55 +0900

    Implement detect_obstacle

--- src/racecar/scripts/obstacle.py
+++ src/racecar/scripts/obstacle.py
@@ -1,8 +1,11 @@
-import threading
 import numpy as np
 
 import rospy
 from std_msgs.msg import LaserScan
+
+lidar_data = None
+rangeH = 0.5
+rangeV = 1.0
 
 
 def lidar_callback(Data):
@@ -10,4 +13,16 @@ def lidar_callback(Data):
 
 
 def detect_obstacle():
-    pass
+    lasdar_sub = rospy.Subscriber('/scan/', LaserScan, lidar_callback)
+
+    if lidar_data is None:
+        return None
+
+    for i in range(10, 170):
+        t = i / 180 * np.pi
+        l = lidar_data.ranges[i]
+
+        if l * np.sin(t) < rangeH and l * np.cos(t) < rangeV:
+            return True
+
+    return False
