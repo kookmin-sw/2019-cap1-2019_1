9484dd69fbfb2de1f6d588ea5163e987ae5cf5fc
thd0222 <thdtjddb@naver.com>
2019-04-18 20:57:29 +0900

    Add image_process, Refactor functions

--- src/racecar/scripts/camtune.py
+++ src/racecar/scripts/camtune.py
@@ -22,6 +22,7 @@ from rosservice import ROSServiceException
 from slidewindow import SlideWindow
 from warper import Warper
 from pidcal import PidCal
+from image_process import *
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
@@ -32,6 +33,7 @@ from ackermann_msgs.msg import AckermannDriveStamped
 warper = Warper()
 slidewindow = SlideWindow()
 pidcal = PidCal()
+processor = ImageProcessor()
 
 q1 = que.Queue()
 bridge = CvBridge()
@@ -39,14 +41,6 @@ bridge = CvBridge()
 cv_image = None
 ack_publisher = None
 car_run_speed = 0.5
-
-# color(yellow)
-lower_color = np.array([0, 150, 30])
-upper_color = np.array([30, 255, 255])
-
-# canny
-low_threshold = 300
-high_threshold = 500
 
 width = 0
 height = 0
@@ -79,23 +73,23 @@ def main():
         width = cv_image.shape[1]
         global height
         height = cv_image.shape[0]
-        yellow_img = preprocessing(cv_image, warping=True, blurring=True)
+        yellow_img = processor.preprocessing(cv_image, warping=True, blurring=True)
         # img1, x_location = process_image(yellow_img)
         cv2.imshow('origin', cv_image)
-        img_lines, lines = find_line(yellow_img)
-        rights_ht, rights_ab = get_right_lines(lines, img_lines)
+        img_lines, lines = processor.find_line(yellow_img)
+        rights_ht, rights_ab = processor.get_right_lines(lines, img_lines)
 
         img_right = yellow_img.copy()
-        draw_line(img_right, rights_ht, 255)
+        processor.draw_line(img_right, rights_ht, 255)
         cv2.imshow('rights', img_right)
 
-        x_location = int(cal_x_location(rights_ab))
+        x_location = processor.cal_x_location(rights_ab)
         img_x_location = yellow_img.copy()
-        cv2.rectangle(img_x_location, (x_location-30, 310), (x_location+30, 370), 255, -1)
+        cv2.rectangle(img_x_location, (x_location - 30, 310), (x_location + 30, 370), 255, -1)
         cv2.imshow('x_location', img_x_location)
-        x_location += int(width * 0.175)
+        x_location = int(x_location + width * 0.175)
 
-        img_circles, circles = find_circle(yellow_img)
+        img_circles, circles = processor.find_circle(yellow_img)
         if x_location is not None:
             pid = round(pidcal.pid_control(int(x_location)), 6)
             print(pid)
@@ -120,110 +114,6 @@ def process_image(frame):
     img1, x_location = slidewindow.slidewindow(edges_img)
 
     return img1, x_location
-
-
-def get_right_lines(lines, img):
-    rights_ht = []
-    rights_ab = []
-    r = lines.T[0]
-    theta = lines.T[1]
-    a = - np.tan(theta)
-    b = np.abs(r / np.cos(theta))
-    top = 0
-    mid = height / 2
-    bot = height
-    mean_top = np.mean(a * top + b)
-    mean_mid = np.mean(a * mid + b)
-    mean_bot = np.mean(a * bot + b)
-
-    cv2.line(img, (mean_top, top), (mean_bot, bot), 128, 2)
-    cv2.imshow('mean_line', img)
-
-    for line in lines:
-        slope = - np.tan(line[0][1])
-        bias = np.abs(line[0][0] / np.cos(line[0][1]))
-        if mean_top < slope * top + bias and mean_mid < slope * mid + bias and mean_bot < slope * bot + bias:
-            rights_ht.append(line)
-            rights_ab.append([slope, bias])
-
-    return np.array(rights_ht), np.array(rights_ab)
-
-
-def cal_x_location(lines_ab):
-    y = 340
-    return np.mean(lines_ab.T[0] * y + lines_ab.T[1])
-
-
-def preprocessing(frame, warping=False, blurring=False):
-    if blurring:
-        img = cv2.GaussianBlur(frame, (5, 5), 0)
-    else:
-        img = frame
-
-    if warping:
-        # warper
-        img = warper.warp(img)
-
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # create mask using color range
-    img_mask = cv2.inRange(img_hsv, lower_color, upper_color)
-    img_mask = cv2.bitwise_and(img, img, mask=img_mask)
-    cv2.imshow("mask", img_mask)
-
-    # mask to gray
-    img_graymask = cv2.cvtColor(img_mask, cv2.COLOR_BGR2GRAY)
-    return img_graymask
-
-
-def draw_line(img, lines, color=None):
-    if color is None:
-        color = 255
-    if lines is not None:
-        for line, i in zip(lines, range(len(lines))):
-            for rho, theta in line:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a * rho
-                y0 = b * rho
-                x1 = int(x0 + 1000 * (-b))
-                y1 = int(y0 + 1000 * (a))
-                x2 = int(x0 - 1000 * (-b))
-                y2 = int(y0 - 1000 * (a))
-
-                cv2.line(img, (x1, y1), (x2, y2), color, 2)
-
-
-def find_line(frame):
-    # canny edge
-    img_canny = cv2.Canny(np.uint8(frame), low_threshold, high_threshold, apertureSize=5)
-    cv2.imshow("canny_line", img_canny)
-
-    # find lines using houghlines and show them
-    lines = cv2.HoughLines(img_canny, 1, np.pi / 180, 100)
-    img = frame.copy()
-    draw_line(img, lines, 255)
-    cv2.imshow("lines", img)
-    return img, lines
-
-
-def find_circle(frame):
-    # canny edge (for debug)
-    # img_canny = cv2.Canny(np.uint8(img_graymask), 10, 20, apertureSize=3)
-    # cv2.imshow("canny_circle", img_canny)
-
-    # find circle using houghcircle
-    img = frame.copy()
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 15, param1=20, param2=10, minRadius=3, maxRadius=10)
-
-    # show circles
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            cv2.circle(img, (i[0], i[1]), i[2], 255, 2)  # circle
-            cv2.circle(img, (i[0], i[1]), 1, 192, 2)  # center
-    cv2.imshow("circles", img)
-    return img, circles
 
 
 if __name__ == '__main__':
---
+++ src/racecar/scripts/image_process.py
@@ -0,0 +1,124 @@
+import cv2
+import numpy as np
+
+from slidewindow import SlideWindow
+from warper import Warper
+
+
+class ImageProcessor:
+    def __init__(self):
+        self.warper = Warper()
+        self.slidewindow = SlideWindow()
+
+        # color(yellow)
+        self.lower_color = np.array([0, 150, 30])
+        self.upper_color = np.array([30, 255, 255])
+
+        # canny
+        self.low_threshold = 300
+        self.high_threshold = 500
+
+    def get_right_lines(self, lines, img, draw_mean_line=False):
+        if lines is None:
+            return None, None
+
+        rights_ht = []
+        rights_ab = []
+        r = lines.T[0]
+        theta = lines.T[1]
+        a = - np.tan(theta)
+        b = np.abs(r / np.cos(theta))
+        top = 0
+        mid = img.shape[0] / 2
+        bot = img.shape[0]
+        mean_top = np.mean(a * top + b)
+        mean_mid = np.mean(a * mid + b)
+        mean_bot = np.mean(a * bot + b)
+
+        if draw_mean_line:
+            cv2.line(img, (mean_top, top), (mean_bot, bot), 128, 2)
+            cv2.imshow('mean_line', img)
+
+        for line in lines:
+            slope = - np.tan(line[0][1])
+            bias = np.abs(line[0][0] / np.cos(line[0][1]))
+            if mean_top < slope * top + bias and mean_mid < slope * mid + bias and mean_bot < slope * bot + bias:
+                rights_ht.append(line)
+                rights_ab.append([slope, bias])
+
+        return np.array(rights_ht), np.array(rights_ab)
+
+    def cal_x_location(self, lines_ab):
+        if lines_ab is None:
+            return None
+
+        y = 340
+        return np.mean(lines_ab.T[0] * y + lines_ab.T[1])
+
+    def preprocessing(self, frame, warping=False, blurring=False):
+        if blurring:
+            img = cv2.GaussianBlur(frame, (5, 5), 0)
+        else:
+            img = frame
+
+        if warping:
+            # warper
+            img = self.warper.warp(img)
+
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # create mask using color range
+        img_mask = cv2.inRange(img_hsv, self.lower_color, self.upper_color)
+        img_mask = cv2.bitwise_and(img, img, mask=img_mask)
+        cv2.imshow("mask", img_mask)
+
+        # mask to gray
+        img_graymask = cv2.cvtColor(img_mask, cv2.COLOR_BGR2GRAY)
+        return img_graymask
+
+    def draw_line(self, img, lines, color=None):
+        if color is None:
+            color = 255
+        if lines is not None:
+            for line, i in zip(lines, range(len(lines))):
+                for rho, theta in line:
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a * rho
+                    y0 = b * rho
+                    x1 = int(x0 + 1000 * (-b))
+                    y1 = int(y0 + 1000 * (a))
+                    x2 = int(x0 - 1000 * (-b))
+                    y2 = int(y0 - 1000 * (a))
+
+                    cv2.line(img, (x1, y1), (x2, y2), color, 2)
+
+    def find_line(self, frame):
+        # canny edge
+        img_canny = cv2.Canny(np.uint8(frame), self.low_threshold, self.high_threshold, apertureSize=5)
+        cv2.imshow("canny_line", img_canny)
+
+        # find lines using houghlines and show them
+        lines = cv2.HoughLines(img_canny, 1, np.pi / 180, 100)
+        img = frame.copy()
+        self.draw_line(img, lines, 255)
+        cv2.imshow("lines", img)
+        return img, lines
+
+    def find_circle(self, frame):
+        # canny edge (for debug)
+        # img_canny = cv2.Canny(np.uint8(img_graymask), 10, 20, apertureSize=3)
+        # cv2.imshow("canny_circle", img_canny)
+
+        # find circle using houghcircle
+        img = frame.copy()
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 15, param1=20, param2=10, minRadius=3, maxRadius=10)
+
+        # show circles
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                cv2.circle(img, (i[0], i[1]), i[2], 255, 2)  # circle
+                cv2.circle(img, (i[0], i[1]), 1, 192, 2)  # center
+        cv2.imshow("circles", img)
+        return img, circles
