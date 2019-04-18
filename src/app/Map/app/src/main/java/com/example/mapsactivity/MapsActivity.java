0de14bf2c1be8daac1d046c8aa2463f9ec90a815
Jongwook-Seong <ypwa121@gmail.com>
2019-04-18 13:48:10 +0900

    Commit Map Activity
    add map function. and add gpstracker

--- src/app/Map/app/src/main/java/com/example/mapsactivity/MapsActivity.java
+++ src/app/Map/app/src/main/java/com/example/mapsactivity/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.mapsactivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -16,17 +17,44 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GPSTracker gps;
+    private ArrayList<Marker> mMarkerList;
+
+    private Button btnMyLocation;
+    private EditText editText;
+
+    private EnterListener enterListener;
+    private String s = "null";
+
+    private static final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        btnMyLocation = (Button)findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.editText);
+        enterListener = new EnterListener();
+        editText.setOnEditorActionListener(enterListener);
 
         requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_REQUEST_CODE);
 
@@ -34,6 +62,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Check and Set GPS & Network Permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        }
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
     }
 
 
@@ -49,6 +88,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        gps = new GPSTracker(MapsActivity.this);
+        mMarkerList = new ArrayList<Marker>();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                String address = getAddress(latLng);
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(address);
+                markerOptions.snippet("latitude: " + latitude.toString() + "\nlongitude: " + longitude.toString());
+
+                Marker marker = mMap.addMarker(markerOptions);
+                mMarkerList.add(marker);
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                for(Marker marker : mMarkerList) {
+                    if(Math.abs(marker.getPosition().latitude - latLng.latitude) < 0.05 && Math.abs(marker.getPosition().longitude - latLng.longitude) < 0.05) {
+                        Toast.makeText(MapsActivity.this, "got clicked", Toast.LENGTH_SHORT).show(); //do some stuff
+                        //mMarkerList.remove(marker);
+                        setLocInfo(marker);
+                        break;
+                    }
+                }
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String coordText = "latitude: " + marker.getPosition().latitude + "\nlongitude: " + marker.getPosition().longitude;
+                Toast.makeText(MapsActivity.this, coordText, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        btnMyLocation.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    Toast.makeText(MapsActivity.this,
+                                    "latitude :" + String.valueOf(latitude) + "\nlongitude :" + String.valueOf(longitude),
+                                    Toast.LENGTH_LONG).show();
+                } else {
+                    gps.showSettingsAlert();
+                }
+            }
+        });
 
         if (mMap != null) {
             mMap.setMyLocationEnabled(true);
@@ -87,6 +183,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
                 return;
             }
+        }
+    }
+
+    private String getAddress(LatLng latLng) {
+        String retAddr = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            Address obj = addresses.get(0);
+            retAddr += obj.getFeatureName() + ": ";
+            retAddr += obj.getAddressLine(0) + " (";
+            retAddr += obj.getPostalCode() + ")";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return retAddr;
+    }
+
+    public void setLocInfo(Marker marker) {
+        String snippet = s;
+        Marker setMarker;
+
+        for (Marker mMarker : mMarkerList) {
+            if (mMarker == marker) {
+                setMarker = marker;
+                setMarker.setSnippet(snippet);
+                mMarkerList.remove(marker);
+                mMarkerList.add(setMarker);
+                break;
+            }
+        }
+    }
+
+    class EnterListener implements EditText.OnEditorActionListener {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            s = v.getText().toString();
+            editText.setText("");
+            return false;
         }
     }
 }
