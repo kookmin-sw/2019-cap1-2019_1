81f40d56ad68d56ad5465e9fd0c25e14dc5c6174
Youngeun Song <youngneee@gmail.com>
2019-05-08 18:29:57 +0900

    Implement str2uuid function

--- src/racecar/bluetooth_test/bluetooth_server.c
+++ src/racecar/bluetooth_test/bluetooth_server.c
@@ -18,7 +18,59 @@ bdaddr_t bdaddr_any = {0, 0, 0, 0, 0, 0};
 bdaddr_t bdaddr_local = {0, 0, 0, 0xff, 0xff, 0xff};
 
 void *ThreadMain(void *argument);
-int _str2uuid( const char *uuid_str, uuid_t *uuid );
+int _str2uuid( const char *uuid_str, uuid_t *uuid ) {
+    /* This is from the pybluez stack */
+
+    uint32_t uuid_int[4];
+    char *endptr;
+
+    if( strlen( uuid_str ) == 36 ) {
+        char buf[9] = { 0 };
+
+        if( uuid_str[8] != '-' && uuid_str[13] != '-' &&
+        uuid_str[18] != '-' && uuid_str[23] != '-' ) {
+        return 0;
+    }
+    // first 8-bytes
+    strncpy(buf, uuid_str, 8);
+    uuid_int[0] = htonl( strtoul( buf, &endptr, 16 ) );
+    if( endptr != buf + 8 ) return 0;
+        // second 8-bytes
+        strncpy(buf, uuid_str+9, 4);
+        strncpy(buf+4, uuid_str+14, 4);
+        uuid_int[1] = htonl( strtoul( buf, &endptr, 16 ) );
+        if( endptr != buf + 8 ) return 0;
+
+        // third 8-bytes
+        strncpy(buf, uuid_str+19, 4);
+        strncpy(buf+4, uuid_str+24, 4);
+        uuid_int[2] = htonl( strtoul( buf, &endptr, 16 ) );
+        if( endptr != buf + 8 ) return 0;
+
+        // fourth 8-bytes
+        strncpy(buf, uuid_str+28, 8);
+        uuid_int[3] = htonl( strtoul( buf, &endptr, 16 ) );
+        if( endptr != buf + 8 ) return 0;
+
+        if( uuid != NULL ) sdp_uuid128_create( uuid, uuid_int );
+    } else if ( strlen( uuid_str ) == 8 ) {
+        // 32-bit reserved UUID
+        uint32_t i = strtoul( uuid_str, &endptr, 16 );
+        if( endptr != uuid_str + 8 ) return 0;
+        if( uuid != NULL ) sdp_uuid32_create( uuid, i );
+    } else if( strlen( uuid_str ) == 4 ) {
+        // 16-bit reserved UUID
+        int i = strtol( uuid_str, &endptr, 16 );
+        if( endptr != uuid_str + 4 ) return 0;
+        if( uuid != NULL ) sdp_uuid16_create( uuid, i );
+    } else {
+        return 0;
+    }
+
+    return 1;
+
+}
+
 sdp_session_t *register_service(uint8_t rfcomm_channel);
 char *read_server(int client);
 void write_server(int client, char *message);
