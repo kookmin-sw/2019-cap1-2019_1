a172ef7e18467c6d85e6d2af1b345b3b2cf7e13a
Youngeun Song <youngneee@gmail.com>
2019-05-08 18:31:01 +0900

    Implement register_service function

--- src/racecar/bluetooth_test/bluetooth_server.c
+++ src/racecar/bluetooth_test/bluetooth_server.c
@@ -71,7 +71,85 @@ int _str2uuid( const char *uuid_str, uuid_t *uuid ) {
 
 }
 
-sdp_session_t *register_service(uint8_t rfcomm_channel);
+sdp_session_t *register_service(uint8_t rfcomm_channel) {
+    const char *service_name = "Armatus Bluetooth server";
+    const char *svc_dsc = "A HERMIT server that interfaces with the Armatus Android app";
+    const char *service_prov = "Armatus";
+
+    uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid,
+           svc_class_uuid;
+    sdp_list_t *l2cap_list = 0,
+                *rfcomm_list = 0,
+                 *root_list = 0,
+                  *proto_list = 0,
+                   *access_proto_list = 0,
+                    *svc_class_list = 0,
+                     *profile_list = 0;
+    sdp_data_t *channel = 0;
+    sdp_profile_desc_t profile;
+    sdp_record_t record = { 0 };
+    sdp_session_t *session = 0;
+
+    // set the general service ID
+    //sdp_uuid128_create(&svc_uuid, &svc_uuid_int);
+    _str2uuid("00001101-0000-1000-8000-00805F9B34FB",&svc_uuid);
+    sdp_set_service_id(&record, svc_uuid);
+
+    char str[256] = "";
+    sdp_uuid2strn(&svc_uuid, str, 256);
+    printf("Registering UUID %s\n", str);
+
+    // set the service class
+    sdp_uuid16_create(&svc_class_uuid, SERIAL_PORT_SVCLASS_ID);
+    svc_class_list = sdp_list_append(0, &svc_class_uuid);
+    sdp_set_service_classes(&record, svc_class_list);
+
+    // set the Bluetooth profile information
+    sdp_uuid16_create(&profile.uuid, SERIAL_PORT_PROFILE_ID);
+    profile.version = 0x0100;
+    profile_list = sdp_list_append(0, &profile);
+    sdp_set_profile_descs(&record, profile_list);
+
+    // make the service record publicly browsable
+    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
+    root_list = sdp_list_append(0, &root_uuid);
+    sdp_set_browse_groups(&record, root_list);
+
+    // set l2cap information
+    sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
+    l2cap_list = sdp_list_append(0, &l2cap_uuid);
+    proto_list = sdp_list_append(0, l2cap_list);
+
+    // register the RFCOMM channel for RFCOMM sockets
+    sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
+    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
+    rfcomm_list = sdp_list_append(0, &rfcomm_uuid);
+    sdp_list_append(rfcomm_list, channel);
+    sdp_list_append(proto_list, rfcomm_list);
+
+    access_proto_list = sdp_list_append(0, proto_list);
+    sdp_set_access_protos(&record, access_proto_list);
+
+    // set the name, provider, and description
+    sdp_set_info_attr(&record, service_name, service_prov, svc_dsc);
+
+    // connect to the local SDP server, register the service record,
+    // and disconnect
+    session = sdp_connect(&bdaddr_any, &bdaddr_local, SDP_RETRY_IF_BUSY);
+    sdp_record_register(session, &record, 0);
+
+    // cleanup
+    sdp_data_free(channel);
+    sdp_list_free(l2cap_list, 0);
+    sdp_list_free(rfcomm_list, 0);
+    sdp_list_free(root_list, 0);
+    sdp_list_free(access_proto_list, 0);
+    sdp_list_free(svc_class_list, 0);
+    sdp_list_free(profile_list, 0);
+
+    return session;
+}
+
 char *read_server(int client);
 void write_server(int client, char *message);
 int main();
