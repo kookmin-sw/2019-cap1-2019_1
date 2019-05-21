de5c580eca98c8216729c25e871a7e4963ed76f0
0is2 <youngneee@gmail.com>
2019-05-21 22:48:00 +0900

    Add print orientation z value

--- src/racecar/imu_test/imu_orientation_test.py
+++ src/racecar/imu_test/imu_orientation_test.py
@@ -5,6 +5,7 @@ def callback(msg):
         print "Quaternoion ========"
         print "x : ", msg.orientation.x
         print "y : ", msg.orientation.y
+        print "z : ", msg.orientation.z
 
 rospy.init_node('get_imu_data')
 sub = rospy.Subscriber('imu', Imu, callback)
